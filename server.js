9c078419720cf186aa6d6a1cf24fe88fc8d1245a
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-09-17 14:27:26 -0300

    aaaa

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
+app.get("/", (res, req) => {
+    return `<h1>Hello, world</h1>`
+})
+
 app.listen("5000", console.log("voov"))
